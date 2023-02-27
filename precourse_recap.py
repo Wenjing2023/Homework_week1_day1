d64edea9e950912cbfbc0f71d3b428062aedd48a
user <user@c016043.wlan.net.ed.ac.uk>
2023-02-27 17:50:37 +0000

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,20 @@
+day_of_week = "Monday"
+current_week = 1
+current_day_of_week = 1
+print("Today is " + day_of_week +", Week " + str(current_week)+
+      " Day " + str(current_day_of_week) + " of CodeClan")
+
+current_week = 1
+current_day_of_week = 1
+total_course_weeks = 16
+total_course_days_per_week = 5
+def weeks_to_go():
+    weeks_left = total_course_weeks - current_week
+    days_left = total_course_days_per_week - current_day_of_week
+    print(f"Only {weeks_left} weeks and {days_left} days to go!")
+
+def motivate_me():
+    print("We got this!!You are doing great!")
+
+weeks_to_go()
+motivate_me()
